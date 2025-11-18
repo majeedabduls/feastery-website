82970a0de941c2a36f1997b9fb2fd5e8f0276f5f
Abdul Majeed <majeedabduls@gmail.com>
2025-11-18 11:10:25 +0530

    moved frontend files to root for netlify

--- feastery-frontend/api.js
+++
@@ -1,9 +0,0 @@
-const API_URL = "https://feastery.onrender.com";
-
-// Get all products
-async function getProducts() {
-    const res = await fetch(`${API_URL}/products/all`);
-    const data = await res.json();
-    return data.products;
-}
-
